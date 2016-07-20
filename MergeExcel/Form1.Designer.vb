7d6c26fe6891b90cb9ec549a541b7da8610d37fb
waixiaoyu <yaoyu_best@163.com>
2016-07-21 00:10:38 +0800

    finished

--- MergeExcel/Form1.Designer.vb
+++ MergeExcel/Form1.Designer.vb
@@ -32,13 +32,19 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.FolderBrowserDialog_Input = New System.Windows.Forms.FolderBrowserDialog()
         Me.FolderBrowserDialog_Output = New System.Windows.Forms.FolderBrowserDialog()
+        Me.TB_OutputFileName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label_Count = New System.Windows.Forms.Label()
+        Me.BTN_Open = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'BTN_Merge
         '
-        Me.BTN_Merge.Location = New System.Drawing.Point(190, 113)
+        Me.BTN_Merge.Location = New System.Drawing.Point(161, 126)
         Me.BTN_Merge.Name = "BTN_Merge"
-        Me.BTN_Merge.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_Merge.Size = New System.Drawing.Size(154, 43)
         Me.BTN_Merge.TabIndex = 1
         Me.BTN_Merge.Text = "开始合并"
         Me.BTN_Merge.UseVisualStyleBackColor = True
@@ -54,7 +60,7 @@ Partial Class Form1
         '
         'BTN_ChooseInput
         '
-        Me.BTN_ChooseInput.Location = New System.Drawing.Point(362, 32)
+        Me.BTN_ChooseInput.Location = New System.Drawing.Point(366, 32)
         Me.BTN_ChooseInput.Name = "BTN_ChooseInput"
         Me.BTN_ChooseInput.Size = New System.Drawing.Size(62, 23)
         Me.BTN_ChooseInput.TabIndex = 10
@@ -65,6 +71,7 @@ Partial Class Form1
         '
         Me.TB_Input.Location = New System.Drawing.Point(161, 34)
         Me.TB_Input.Name = "TB_Input"
+        Me.TB_Input.ReadOnly = True
         Me.TB_Input.Size = New System.Drawing.Size(195, 21)
         Me.TB_Input.TabIndex = 9
         '
@@ -79,7 +86,7 @@ Partial Class Form1
         '
         'BTN_ChooseOutput
         '
-        Me.BTN_ChooseOutput.Location = New System.Drawing.Point(362, 66)
+        Me.BTN_ChooseOutput.Location = New System.Drawing.Point(366, 66)
         Me.BTN_ChooseOutput.Name = "BTN_ChooseOutput"
         Me.BTN_ChooseOutput.Size = New System.Drawing.Size(62, 23)
         Me.BTN_ChooseOutput.TabIndex = 13
@@ -90,6 +97,7 @@ Partial Class Form1
         '
         Me.TB_Output.Location = New System.Drawing.Point(107, 68)
         Me.TB_Output.Name = "TB_Output"
+        Me.TB_Output.ReadOnly = True
         Me.TB_Output.Size = New System.Drawing.Size(249, 21)
         Me.TB_Output.TabIndex = 12
         '
@@ -102,11 +110,69 @@ Partial Class Form1
         Me.Label1.TabIndex = 15
         Me.Label1.Text = "将需要合并的Excel放在同一个目录下即可"
         '
+        'TB_OutputFileName
+        '
+        Me.TB_OutputFileName.Location = New System.Drawing.Point(143, 99)
+        Me.TB_OutputFileName.Name = "TB_OutputFileName"
+        Me.TB_OutputFileName.Size = New System.Drawing.Size(172, 21)
+        Me.TB_OutputFileName.TabIndex = 16
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(125, 12)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "输出文件名（可选）："
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(315, 102)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(35, 12)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = ".xlsx"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(354, 102)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(113, 12)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "(默认为：new.xlsx)"
+        '
+        'Label_Count
+        '
+        Me.Label_Count.AutoSize = True
+        Me.Label_Count.Location = New System.Drawing.Point(334, 141)
+        Me.Label_Count.Name = "Label_Count"
+        Me.Label_Count.Size = New System.Drawing.Size(0, 12)
+        Me.Label_Count.TabIndex = 20
+        '
+        'BTN_Open
+        '
+        Me.BTN_Open.Enabled = False
+        Me.BTN_Open.Location = New System.Drawing.Point(47, 136)
+        Me.BTN_Open.Name = "BTN_Open"
+        Me.BTN_Open.Size = New System.Drawing.Size(90, 23)
+        Me.BTN_Open.TabIndex = 21
+        Me.BTN_Open.Text = "打开合并结果"
+        Me.BTN_Open.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(504, 166)
+        Me.ClientSize = New System.Drawing.Size(467, 181)
+        Me.Controls.Add(Me.BTN_Open)
+        Me.Controls.Add(Me.Label_Count)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TB_OutputFileName)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.BTN_ChooseOutput)
@@ -131,5 +197,11 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents FolderBrowserDialog_Input As System.Windows.Forms.FolderBrowserDialog
     Friend WithEvents FolderBrowserDialog_Output As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents TB_OutputFileName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label_Count As System.Windows.Forms.Label
+    Friend WithEvents BTN_Open As System.Windows.Forms.Button
 
 End Class
--- MergeExcel/Form1.vb
+++ MergeExcel/Form1.vb
@@ -1,32 +1,148 @@
 ﻿Imports Microsoft.Office.Interop
+Imports System.IO
 
 Public Class Form1
 
-
-
-
-
+    Private Sub BTN_ChooseInput_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BTN_ChooseInput.Click
+        FolderBrowserDialog_Input.ShowDialog()
+        TB_Input.Text = FolderBrowserDialog_Input.SelectedPath
+        inputPath = FolderBrowserDialog_Input.SelectedPath
+    End Sub
     Private Sub BTN_ChooseOutput_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BTN_ChooseOutput.Click
         FolderBrowserDialog_Output.ShowDialog()
         TB_Output.Text = FolderBrowserDialog_Output.SelectedPath
+        outputPath = FolderBrowserDialog_Input.SelectedPath
     End Sub
-
+    Dim inputPath As String
+    Dim outputPath As String
+    Dim outputFileName As String = "\new.xlsx"
+    Dim totalCount As Int16
+    Dim currentCount As Int16 = 1
     Private Sub BTN_Merge_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BTN_Merge.Click
+        BTN_Open.Enabled = False
+        Label_Count.Text = "准备开始..."
+        If TB_OutputFileName.Text <> Nothing Then
+            outputFileName = "\" + TB_OutputFileName.Text + ".xlsx"
+        End If
+        xlRows = 2
+
+        If (inputPath = Nothing Or outputPath = Nothing) Then
+            MessageBox.Show("请先选择输入输出路径！")
+            Return
+        End If
+        createNewExcel()
+        Dim dir As System.IO.DirectoryInfo = New DirectoryInfo(inputPath)
+        totalCount = dir.GetFiles("*.xlsx", SearchOption.AllDirectories).Length
+        For Each item As System.IO.FileInfo In dir.GetFiles("*.xlsx", SearchOption.AllDirectories)
+            If (item.Attributes And FileAttributes.Hidden) <> FileAttributes.Hidden Then '除去隐藏文件
+                ''进度展示
+                Label_Count.Text = "正在进行..." + CStr(currentCount) + "/" + CStr(totalCount)
+                currentCount += 1
+                ''
+                Dim myExcel As New Excel.Application()
+                Dim workBook As Excel.Workbook '定义工作簿
+                Dim sheet As Excel.Worksheet '定义工作表
+                Dim columns As Int16
+                Dim rows As Int16
+                myExcel.Visible = False
+                ''
+                workBook = myExcel.Workbooks.Open(item.FullName)
+                sheet = workBook.Sheets(1)
+                columns = sheet.UsedRange.Cells.Columns.Count
+                rows = sheet.UsedRange.Cells.Rows.Count
+                For i = 2 To rows
+                    xlColumns = 1
+                    For j = 1 To columns
+                        xlSheet.Cells(xlRows, xlColumns).value = sheet.Cells(i, j).value '往新文件中写入数据
+                        xlColumns += 1
+                    Next
+                    xlRows += 1
+                Next
+                sheet = Nothing
+                workBook.Close(True)
+                myExcel.Quit()
+                workBook = Nothing
+                myExcel = Nothing
+                ''
+            End If
+        Next
+
+        ''''''''''''''''''''''''''''''补充表头
+        Dim di As IO.DirectoryInfo = New IO.DirectoryInfo(inputPath)
+        ''取所有文件
+        Dim fii() As IO.FileInfo = di.GetFiles("*.xlsx", IO.SearchOption.AllDirectories)
+        createHeader(fii)
+        '''''''''''''''''''''''''''''''''''''''
+
+        closeNewExcel()
+        Label_Count.Text = "完成！"
+
+        MessageBox.Show("合并完成！")
+        BTN_Open.Enabled = True
+        ProcessKill()
+        GC.Collect()
+
+    End Sub
+    Dim xlApp As New Excel.Application
+    Dim xlBook As Excel.Workbook
+    Dim xlSheet As Excel.Worksheet
+    Dim xlColumns As Int16 = 1
+    Dim xlRows As Int16 = 2
+    Private Sub createNewExcel()
+        '新建excel
+        xlApp = CType(CreateObject("Excel.Application"), Excel.Application) '创建EXCEL对象 
+        xlBook = xlApp.Workbooks.Add()
+        xlSheet = CType(xlBook.Worksheets("sheet1"), Excel.Worksheet) '设置活动工作表 
+    End Sub
+    Private Sub createHeader(ByVal fii() As IO.FileInfo)
         Dim myExcel As New Excel.Application()
         Dim workBook As Excel.Workbook '定义工作簿
         Dim sheet As Excel.Worksheet '定义工作表
+        Dim columns As Int16
+        Dim rows As Int16
         myExcel.Visible = False
-        workBook = myExcel.Workbooks.Open("C:\Users\Administrator\Desktop\1.xlsx")
+        ''
+        workBook = myExcel.Workbooks.Open(fii(0).FullName)
         sheet = workBook.Sheets(1)
-
-
-        MessageBox.Show(sheet.Cells(1, 2).value())
-
-
-
+        columns = sheet.UsedRange.Cells.Columns.Count
+        rows = sheet.UsedRange.Cells.Rows.Count
+        xlColumns = 1
+        For j = 1 To columns
+            xlSheet.Cells(1, xlColumns).value = sheet.Cells(1, j).value '往新文件中写入数据
+            xlColumns += 1
+        Next
+        sheet = Nothing
+        workBook.Close(True)
         myExcel.Quit()
+        workBook = Nothing
+        myExcel = Nothing
+    End Sub
+    Private Sub closeNewExcel()
+        ''关闭新文件
+        xlBook.SaveAs(outputPath + outputFileName)
+        xlSheet = Nothing
+        xlBook.Close(True)
+        xlApp.Quit()
+        xlBook = Nothing
+        xlApp = Nothing
+    End Sub
+    Private Sub ProcessKill()
+        Dim p As System.Diagnostics.Process
+        p = New System.Diagnostics.Process
+        For Each p In System.Diagnostics.Process.GetProcesses()
+            If p.ProcessName.ToUpper() = "EXCEL" Then
+                p.Kill()
+            End If
+        Next
+    End Sub
 
 
-
+    Private Sub BTN_Open_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BTN_Open.Click
+        Dim myExcel As New Excel.Application()
+        Dim workBook As Excel.Workbook '定义工作簿
+        myExcel.Visible = True
+        ''
+        workBook = myExcel.Workbooks.Open(outputPath + outputFileName)
+        BTN_Open.Enabled = False
     End Sub
 End Class
